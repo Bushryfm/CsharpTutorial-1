dc3ccfaf10a319e774630de86531d3699ffab2e4
F m Bushry <121561467+Bushryfm@users.noreply.github.com>
2024-08-15 22:13:15 +0530

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Mathematics.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpTutorial
+{
+    internal class Mathematics
+    {
+        internal int Sub(int a, int b)
+        {
+
+            return a - b;
+        
+        }
+
+
+    }
+
+  
+
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CsharpTutorial;
+
+Mathematics obj = new Mathematics();
+int total = obj.Sub(100, 200);
+
+Console.WriteLine(total);
